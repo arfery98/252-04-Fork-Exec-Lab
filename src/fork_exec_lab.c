793dea51b5ecb7d81a8e4b99e3b849fb26a7bad9
AFERY_RS <arferyrs@gmail.com>
2026-08-31 19:55:29 +0700

    Homework Finished

--- src/fork_exec_lab.c
+++ src/fork_exec_lab.c
@@ -12,23 +12,29 @@ pid_t spawn_child(void) {
 }
 
 int run_child_process(int *heap_counter, int stack_counter) {
-    int child_sum;
+    if (heap_counter == NULL) {
+        return 1;
+    }
 
-    /* TODO(student): Update all three counters in the child by +7. */
-    /* TODO(student): Compute child_sum as the sum of the updated counters. */
-    /* TODO(student): Print exactly: child: g=<g> h=<h> s=<s> sum=<sum> */
-    /* TODO(student): Return child_sum % 256. */
+    g_counter += 7;
+    *heap_counter += 7;
+    stack_counter += 7;
 
-    (void)heap_counter;
-    (void)stack_counter;
-    (void)child_sum;
-    fprintf(stderr, "TODO: child address-space logic not implemented\n");
-    return 1;
+    int child_sum = g_counter + *heap_counter + stack_counter;
+
+    printf("child: g=%d h=%d s=%d sum=%d\n", g_counter, *heap_counter, stack_counter, child_sum);
+    fflush(stdout);
+
+    return child_sum % 256;
 }
 
 int wait_for_child(pid_t child_pid, int *heap_counter, int stack_counter) {
     int status = 0;
     int child_code;
+
+    if (heap_counter == NULL) {
+        return 1;
+    }
 
     if (waitpid(child_pid, &status, 0) == -1) {
         perror("waitpid");
@@ -42,18 +48,49 @@ int wait_for_child(pid_t child_pid, int *heap_counter, int stack_counter) {
 
     child_code = WEXITSTATUS(status);
 
-    /* TODO(student): Print exactly: parent: child-exit=<code> */
-    /* TODO(student): Print exactly: parent: g=<g> h=<h> s=<s> */
-    /* TODO(student): If parent values are 100, 200, 300 print:
-       parent: address-space=isolated
-       Otherwise print:
-       parent: address-space=unexpected */
-    /* TODO(student): Return 0 only if child_code is 109 and parent values are unchanged. */
+    printf("parent: child-exit=%d\n", child_code);
+    printf("parent: g=%d h=%d s=%d\n", g_counter, *heap_counter, stack_counter);
 
-    (void)status;
-    (void)child_code;
-    (void)heap_counter;
-    (void)stack_counter;
-    fprintf(stderr, "TODO: parent reporting not implemented\n");
+    if (g_counter == 100 && *heap_counter == 200 && stack_counter == 300) {
+        printf("parent: address-space=isolated\n");
+    } else {
+        printf("parent: address-space=unexpected\n");
+    }
+    fflush(stdout);
+
+    if (child_code == 109 && g_counter == 100 && *heap_counter == 200 && stack_counter == 300) {
+        return 0;
+    }
+
     return 1;
+}
+
+int main(void) {
+    int *heap_counter = (int *)malloc(sizeof(int));
+    if (heap_counter == NULL) {
+        perror("malloc");
+        return 1;
+    }
+    *heap_counter = 200;
+    int stack_counter = 300;
+
+    printf("parent: start g=%d h=%d s=%d\n", g_counter, *heap_counter, stack_counter);
+    fflush(stdout);
+
+    pid_t pid = spawn_child();
+    if (pid < 0) {
+        perror("fork");
+        free(heap_counter);
+        return 1;
+    }
+
+    if (pid == 0) {
+        int child_code = run_child_process(heap_counter, stack_counter);
+        free(heap_counter);
+        exit(child_code);
+    }
+
+    int res = wait_for_child(pid, heap_counter, stack_counter);
+    free(heap_counter);
+    return res;
 }
